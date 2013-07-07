352548a110fd53b3687f4abf22512887c20057d1
unknown <mashadar@MOBILESMM-PC.intranet.mediber.de>
2013-07-07 20:15:01 +0200

    Animal beispielhaft mit einem normalen und einem aktivierten Bild und mit einer auf Klick/Touch reagierenden Animation versehen (geht iwi nur einmal)

--- MeediKids/Animal.xaml.cs
+++ MeediKids/Animal.xaml.cs
@@ -10,6 +10,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -22,6 +23,13 @@ namespace MeediKids
         public Animal()
         {
             this.InitializeComponent();
+        }
+
+        private void OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Clicked", true);
+            //var uri = new System.Uri("ms-appx:///Assets/Animals/animal_clicked.png");
+            //AnimalImage.Source = new BitmapImage(uri);
         }
     }
 }
